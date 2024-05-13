ee71f31976782420b170dc5e150485ab95c7c4c9
cetinhakan39 <cetinhakan39@gmail.com>
2024-05-13 21:05:36 +0300

    Initial Commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // IBOutlet allows me to referencee a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+
+    @IBAction func button(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        /* Bu şekilde de yapabilirim
+         
+         diceImageView1.image = diceArray[Int.random(in: 0...5)]
+         diceImageView2.image = diceArray[Int.random(in: 0...5)]
+         
+         diceImageView1.image = diceArray.randomElement()
+         diceImageView2.image = diceArray.randomElement()
+         
+         */
+
+    }
+    
+   
+}
+
